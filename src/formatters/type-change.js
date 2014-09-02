b51022714121b1e5ff44d517bff18dfe1a5ac7c7
Martin Czuchra <martin.czuchra@googlemail.com>
2014-09-02 14:24:45 +0200

    Adds quotation via FormatBar.

--- src/formatters/type-change.js
+++ src/formatters/type-change.js
@@ -50,6 +50,51 @@
 
   });
 
+  var Quote = SirTrevor.Formatter.extend({
+    title: "quote",
+    iconName: "quote",
+    text : "“",
+
+    prepare: function() {
+      var selection = window.getSelection();
+
+      if (selection.rangeCount === 0) {
+        SirTrevor.log("Can't get current selection from formatter!");
+        return;
+      }
+
+      var node = $(selection.getRangeAt(0)
+                   .startContainer
+                   .parentNode)
+                 .parents(".st-block")
+                 .first();
+
+      this._instance = SirTrevor.getInstance(node.attr('data-instance'));
+      this._block = this._instance.getBlocksByIDs( [node.attr('id')] ) [0];
+    },
+
+    getCurrentBlock: function() {
+      return this._block;
+    },
+
+    getCurrentInstance: function() {
+      return this._instance;
+    },
+
+    onClick: function() {
+      var instance = this.getCurrentInstance();
+      var block = this.getCurrentBlock();
+      instance.changeBlockType(block, block.type === "quote" ? "text" : "quote");
+    },
+
+    isActive: function() {
+      this.prepare();
+      return this.getCurrentBlock().type === "quote";
+    }
+
+  });
+
   SirTrevor.Formatters.Heading = new Heading();
+  SirTrevor.Formatters.Quote = new Quote();
 
 })();
