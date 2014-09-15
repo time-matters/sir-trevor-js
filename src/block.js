82e9a52050f3338d54b16a2acd30839ef05838d4
Darren Cadwallader <darren@bitcrowd.net>
2014-09-15 11:44:47 +0200

    [#78729256] Style trevor block deletion.

--- src/block.js
+++ src/block.js
@@ -6,11 +6,11 @@ SirTrevor.Block = (function(){
 
   var delete_template = [
     "<div class='st-block__ui-delete-controls'>",
+      "<a class='btn--confirm-delete st-icon' data-icon='tick'></a>",
+      "<a class='btn--deny-delete st-icon' data-icon='close'></a>",
       "<label class='st-block__delete-label'>",
       "<%= i18n.t('general:delete') %>",
       "</label>",
-      "<a class='btn--confirm-delete st-icon' data-icon='tick'></a>",
-      "<a class='btn--deny-delete st-icon' data-icon='close'></a>",
     "</div>"
   ].join("\n");
 
@@ -229,7 +229,7 @@ SirTrevor.Block = (function(){
         return;
       }
 
-      this.$inner.append(_.template(delete_template));
+      this.$inner.find('.btn--editor-panel--delete').append(_.template(delete_template));
       this.$el.addClass('st-block--delete-active');
 
       var $delete_el = this.$inner.find('.st-block__ui-delete-controls');
