fe0c3dc314a3f57bf251c2e5d5fc2abafde83d3b
Nils Kenneweg <nk@ovt.me>
2015-02-11 16:11:45 +0100

    compile files.

--- sir-trevor.js
+++ sir-trevor.js
@@ -3581,15 +3581,6 @@
   })();
   SirTrevor.Blocks.ExtendedTweet = (function(){
   
-    var tweet_template = _.template([
-      "<blockquote class='twitter-tweet' align='center'>",
-      "<p><%= text %></p>",
-      "&mdash; <%= user.name %> (@<%= user.screen_name %>)",
-      "<a href='<%= status_url %>' data-datetime='<%= created_at %>'><%= created_at %></a>",
-      "</blockquote>",
-      '<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
-    ].join("\n"));
-  
     return SirTrevor.Block.extend({
       providers: {
         soundcloud: {
@@ -4034,6 +4025,108 @@
       this.getBlock().text(data.text);
     }
   });
+  SirTrevor.Blocks.Instagram = (function(){
+  
+    return SirTrevor.Block.extend({
+      providers: {
+        instagram: {
+          // http://instagram.com/p/bNd86MSFv6/
+          regex: /((?:http[s]?:\/\/)?(?:www.)?(:?instagr.*\/p\/(.*?)))(:?\?.*)?$/,
+          html: function(options, success, error) {
+  
+            // Cross site requests will not produce errors that jQuery
+            // could catch. We need a timeout that is cleared when the
+            // request was successful.
+            var fail = window.setTimeout(error, 5000);
+  
+            // Using YQL and JSONP
+            $.ajax({
+              url: "http://api.instagram.com/oembed",
+              data: {
+                maxwidth: 550,
+                url: options.remote_id
+              },
+              // The name of the callback parameter, as specified by the YQL service
+              jsonp: "callback",
+              dataType: "jsonp"
+            }).done(function(data) {
+              // clear the error timeout.
+              clearTimeout(fail);
+              success(data.html, options);
+            });
+          }
+        }
+      },
+  
+      type: 'instagram',
+      title: function() { return i18n.t('blocks:instagram:title'); },
+  
+      droppable: true,
+      pastable: true,
+  
+      removeEmpty: true,
+  
+      icon_name: 'twitter-outline',
+  
+      extractSourceInformation: function(options) {
+        var url = options.remote_id;
+        this.$editor.parents('.st-block').append(
+          '<aside>' + i18n.t('general:source') + ': ' + url + '</aside>');
+      },
+  
+      loadData: function(data){
+        var embed_string, self = this;
+  
+        var update_editor = function(embed_string, options) {
+          self.$editor.html(embed_string);
+          self.extractSourceInformation(options);
+        };
+  
+        var display_error = function() {
+          embed_string = '<h1><i class="icon--exclamation-triangle"></i></h1>';
+          self.$editor.html(embed_string);
+        };
+  
+        var html = this.providers[data.source].html;
+        html({
+          protocol: window.location.protocol,
+          remote_id: data.remote_id,
+          width: this.$editor.width()
+        }, update_editor, display_error);
+      },
+  
+      onContentPasted: function(event){
+        this.handleDropPaste($(event.target).val());
+      },
+  
+      handleDropPaste: function(url){
+        if(!_.isURI(url)) {
+          return;
+        }
+  
+        var match, data;
+  
+        _.each(this.providers, function(provider, index) {
+          match = provider.regex.exec(url);
+  
+          if(match !== null && !_.isUndefined(match[1])) {
+            data = {
+              source: index,
+              remote_id: match[1]
+            };
+  
+            this.setAndLoadData(data);
+          }
+        }, this);
+      },
+  
+      onDrop: function(transferData){
+        var url = transferData.getData('text/plain');
+        this.handleDropPaste(url);
+      }
+    });
+  
+  })();
 
   /* dynamic Blocks */
   SirTrevor.DynamicBlocks.RemoteSelectableList = (function(){
