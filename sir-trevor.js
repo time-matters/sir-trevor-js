8884deceff368556ffee416b7ed5aa4c8f0839f6
Tobias Pfeiffer <tobi@bitcrowd.net>
2015-03-02 12:43:44 +0100

    Moare Facebook URLs and specs

--- sir-trevor.js
+++ sir-trevor.js
@@ -3731,7 +3731,11 @@
       '<div class="fb-post" data-href="<%= url %>" data-width="<%= width %>"></div>'
     ].join("\n"));
   
-    var facebookPostRegex = /((?:http[s]?:\/\/)?(?:www.)?(:?facebook.com\/.*\/posts\/(.*?)))(:?\?.*)?$/;
+    // https://www.facebook.com/video.php?v=932260686798181
+    // https://www.facebook.com/mehrbasketball/posts/789246717829360
+    // https://www.facebook.com/BuschmannFrank/photos/a.646326722058247.1073741825.117176518306606/935661633124753/?type=1
+    // https://www.facebook.com/photo.php?fbid=923923527651763&set=a.340340156010106.85330.100001023720602&type=1
+    var facebookPostRegex = /((?:http[s]?:\/\/)?(?:www.)?(:?facebook.com\/(video\.php|photo\.php|(.*\/(posts|photos)\/))(.*?)))(:?\?.*)?$/;
   
     return SirTrevor.Block.extend({
   
